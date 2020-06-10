82a4a2ef42e20a76d7ecdb71d505113ed011de32
Marcelo Melo <mm.ifce@gmail.com>
2020-06-10 16:57:37 -0300

    refresh migration user

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -7,10 +7,11 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.string('username', 80).notNullable()
+      table.string('cpf', 25).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
-      table.boolean('admin')
+      table.boolean('admin').defaultTo(false)
       table.string('token')
       table.timestamp('token_created_at')
       table.timestamps()
